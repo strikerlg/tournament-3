438ac5047b19a954e7b5c169ddf1fbddb34b837a
agloa <cio@agloa.org>
2015-12-25 14:23:27 -0600

    send members to basic template

--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/CRM/Tournament/Page/Registration.php
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/CRM/Tournament/Page/Registration.php
@@ -8,81 +8,94 @@ class TournamentObject{
 	 *
 	 * @var bool
 	 */
-	protected $is_error;
+	public $is_error;
 	/**
 	 *
 	 *
 	 * @var string
 	 */
-	protected $error_message;
+	public $error_message;
 	
 	function __construct(){
 		$this->is_error = 0;
 	}	
 	
-// 	private static $tournament_event_type_id;
-// 	public static function tournament_event_type_id() {
-// 		if (!isset($tournament_event_type_id)) {			
-// 			$tournament_event_type_id = getOptionValue('event_type', 'Tournament');
-// 		}
-// 		return $tournament_event_type_id;
-// 	}	
-	
-	private static $billing_contact_relationship_type_id;
-	public static function billing_contact_relationship_type_id() {
-		if (!isset($billing_contact_relationship_type_id))
-			$billing_contact_relationship_type_id = TournamentObject::relationship_type_id('Billing Contact for');
-		return $billing_contact_relationship_type_id;
+	function member_relationship_type_id() {
+		return $this->relationship_type_id('Member of');
 	}
 	
-	public static function relationship_type_id($name){
-		$apiParams = array('name_a_b' => $name);
-		$records = TournamentObject::civicrm_api3_get('Relationship Type', $apiParams);
-		foreach ($records as $record) return $record['id']; 
+	function tournament_event_type_id(){		
+		return $this->option_value('event_type', 'Tournament');
 	}
 	
-	private static $member_relationship_type_id;
-	public static function member_relationship_type_id($name) {
-		if (!isset($member_relationship_type_id))
-			$member_relationship_type_id = TournamentObject::relationship_type_id('Participates for');
-		return $member_relationship_type_id;
+	function option_value($groupName, $name){
+		$groupID = $this->option_group_id($groupName);
+		$apiParams = array('option_group_id' => $groupID, 'name' => $name);
+		$table = 'Option Value';
+		$record = $this->civicrm_api3_get($table, $apiParams, 'getsingle');
+		return $record['value']; 
 	}
 	
-	public static function get_contact($contact_id){
-		$apiParams = array('id' => $contact_id);//var_dump($apiParams); die('48');
-		$api = new civicrm_api3();
-		if ($api->Contact->get($apiParams)) {
-			return $api->values[0];
-		} else {
-			return $api->error_message();
-		}
+	function option_group_id($name){
+		$apiParams = array('name' => $name);
+		return $this->table_name_to_id('Option Group', $name);
+	}
+	
+	function table_name_to_id($table, $name){
+		$apiParams = array('name' => $name);
+		return $this->table_record_id($table, $apiParams);
+	}
+	
+	function table_record_id($table, $apiParams){
+		$record = $this->civicrm_api3_get($table, $apiParams, 'getsingle');
+		return $record['id'];
+	}
+	
+	function get_contact($contact_id){
+		return $this->civicrm_api3_get('Contact', array('id' => $contact_id), 'getsingle');
 	}
 	
 	function get_active_relationships($cid = null,  $apiParams) {
-		return TournamentObject::get_relationships($cid, 1,  $apiParams);
+		return $this->get_relationships($cid, 1,  $apiParams);
 	}
 		
 	function get_active_b_relationships($cid = null, $apiParams) {
-		return TournamentObject::get_relationships($cid, 1, $apiParams, 'contact_id_b');
+		return $this->get_relationships($cid, 1, $apiParams, 'contact_id_b');
 	}
 		
 	function get_relationships($cid = null, $is_active = 0, $apiParams, $field = 'contact_id_a') {
 		if (isset($cid)){
-		  $apiParams = TournamentObject::params_merge($apiParams, array($field => $cid));
+		  $apiParams = $this->params_merge($apiParams, array($field => $cid));
 		}
 		
-		$apiParams = TournamentObject::params_merge($apiParams, array('is_active' => $is_active));
-		return TournamentObject::civicrm_api3_get('Relationship', $apiParams);
+		$apiParams = $this->params_merge($apiParams, array('is_active' => $is_active));
+		return $this->civicrm_api3_get('Relationship', $apiParams);
 	}
 	
-	function params_merge($apiParams, $params){
-		if (isset($apiParams)) return array_merge($apiParams, $params);
-		else return $params;
+	function get_members($organization){
+		$apiParams = array('relationship_type_id' => $this->member_relationship_type_id());
+		
+		$relations = $this->get_active_b_relationships($organization['id'], $apiParams);
+		$members = array();
+		foreach ($relations as $relation){
+			$member = $this->get_contact($relation['contact_id_a']);
+			$members[$member['id']] = $member;
+		}
+		return $members;
+	}
+
+	function relationship_type_id($name){
+		$record = $this->civicrm_api3_get('Relationship Type', array('name_a_b' => $name), 'getsingle');
+		return $record['id'];
 	}
 	
-	function civicrm_api3_get($tableName, $apiParams) {	
+	function civicrm_api3_get($tableName, $apiParams, $action = 'get') {	
 		try{
-			$result = civicrm_api3($tableName, 'get', $apiParams);
+			if ($action == 'getsingle') 
+				$apiParams['options'] = $this->params_merge($apiParams['options'], array('limit' => 1));
+			
+			$result = civicrm_api3($tableName, $action, $apiParams);
+			if ($action == 'getsingle') return $result;
 			return $result[values];
 		}
 		catch (CiviCRM_API3_Exception $e) {
@@ -94,52 +107,10 @@ class TournamentObject{
 		}
 	}
 	
-	public static function get_members($organization){
-		$apiParams = array('relationship_type_id' => TournamentObject::member_relationship_type_id());
-		
-		$relations = TournamentObject::get_active_b_relationships($organization->id, $apiParams);
-		$members = array();
-		foreach ($relations as $relation){
-			$member = TournamentObject::get_contact($relation['contact_id_a']);
-			$members[$member->id] = $member;
-		}
-		return $members;
+	function params_merge($oldParams, $newParams){
+		if (isset($oldParams)) return array_merge($oldParams, $newParams);
+		else return $newParams;
 	}
-	
-	public static function get_billing_organization($contact){
-		$apiParams = array('relationship_type_id' => TournamentObject::billing_contact_relationship_type_id()
-				, 'options' => array( 'sort' => 'start_date DESC')
-		);
-		
-		$relations = TournamentObject::get_active_relationships($contact->id, $apiParams);
-		foreach ($relations as $relation)
-			$org = TournamentObject::get_contact($relation['contact_id_b']);
-		return $org;
-	}
-	
-	public static function getOptionValue($optionGroupName, $optionValueName){
-		$option_group_id = getActiveNamedTableValue('option_group', $optionGroupName, 'id');
-		$params = array('option_group_id' => $option_group_id);
-		return getActiveNamedTableValue('option_value', $optionValueName, 'value', $params);
-	}
-	
-// 	public static function getActiveNamedTableValue($table, $name, $valueKey = 'value', $params = null) {
-// 		$api = new civicrm_api3();
-			
-// 		$apiParams = array( 'name' => $name, 'is_active' => 1);
-			
-// 		if (isset($params)) $apiParams = array_merge($apiParams, $params);
-	
-// 		if ($api->$table->get($apiParams)) {
-// 			$result = $api->values[0];
-// 		} else {
-// 			return $api->error_message();
-// 		}
-	
-// 		if (is_array($result)) foreach($result['values'] as $value) $result_value = $value[$valueKey];
-// 		else $result_value = $result->$valueKey;
-// 		return $result_value;
-// 	}
 }
 
 class Tournament extends TournamentObject {
@@ -151,21 +122,19 @@ class Tournament extends TournamentObject {
 	public $event;
 	function __construct(){	
 		parent::__construct();	
-		$api = new civicrm_api3();
-		
+
 		// get current tournament, i.e., latest active event of type 'Tournament'
 		$apiParams = array(
-				'event_type_id' => $tournament_event_type_id
+				'event_type_id' => $this->tournament_event_type_id()
 				, 'is_active' => 1
 				, 'options' => array( 'sort' => 'start_date DESC')
 		);
 		
-		$table = 'Event';
-		if ($api->$table->get($apiParams)) {
-			$this->event = $api->values[0];
-		} else {
-			$this->is_error = 1;
-			$this->error_message = $api->error_message();
+		$this->event = $this->civicrm_api3_get('Event', $apiParams, 'getsingle');
+		
+		if (isset($this->event['error'])) {
+			$this->is_error = true;
+			$this->error_message = $this->event['error'];
 		}
 	}
 }
@@ -196,13 +165,30 @@ class BillingContact extends TournamentObject{
 	 */
 	public $members;
 	
+	private $billing_contact_relationship_type_id;
+	
 	function __construct($contact_id) {
 		parent::__construct();
 		
 		$this->tournament = new Tournament();
-		$this->contact = TournamentObject::get_contact($contact_id);
-		$this->organization = TournamentObject::get_billing_organization($this->contact);
-		$this->members = TournamentObject::get_members($this->organization);
+		if ($this->tournament->is_error) {
+			$this->is_error = true;
+			$this->error_message = $this->tournament->error_message;
+			return;
+		}
+		
+		$this->contact = $this->get_contact($contact_id);
+		$this->organization = $this->get_billing_organization();
+		$this->members = $this->get_members($this->organization);
+	}
+	
+	function get_billing_organization(){
+		$apiParams = array('relationship_type_id' => $this->relationship_type_id('Billing Contact for')
+				, 'options' => array( 'sort' => 'start_date DESC', 'limit' => 1)
+		);
+	
+		$relations = $this->get_active_relationships($this->contact['id'], $apiParams);
+		foreach ($relations as $relation) return $this->get_contact($relation['contact_id_b']);
 	}
 }
 
@@ -218,59 +204,12 @@ class CRM_Tournament_Page_Registration extends CRM_Core_Page {
     }
     
     $billing_contact = new BillingContact($billing_contact_id); //var_dump($billing_contact); //die('128');
-    
+    if ($billing_contact->is_error) var_dump($billing_contact);
+     
     $this->assign('billing_contact', $billing_contact);
-
-    //$this->registration_group_contact_get($contact_id); 
-
-    // get assigned registration groups : search for contact in groups of type Registration group
-    // Find all of the groups contacts
 
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     //CRM_Utils_System::setTitle(ts('Registration'));
     parent::run();
-  }
-
-  function registration_group_contact_get($contact_id){
-    $option_group_id = $this->getActiveNamedTableValue('option_group', 'group_type', 'id');
-    $params = array('option_group_id' => $option_group_id);
-
-    // Expected value = 4
-    $registration_group_type = $this->getActiveNamedTableValue('option_value', 'Registration Group', 'value', $params); //var_dump($registration_group_type); die('72');
-
-    $contact_groups = $this->group_contact_get($contact_id); //var_dump($contact_groups);
-    $registrationGroups = array();
-    foreach($contact_groups['values'] as $contact_group){
-      $params = array('id' => $contact_group["group_id"], 'is_active' => 1, 'group_type' => $registration_group_type);
-      $registrationGroups = array_merge($registrationGroups, civicrm_api3('group', 'get', $params)); 
-    }
-    
-    $registrationGroupNames = array();
-    foreach($registrationGroups as $group) foreach($group as $groupElement) $registrationGroupNames = array_merge($registrationGroupNames, $groupElement['description']);
-
-    $this->assign('registration_groups', $registrationGroups);
-    $this->assign('registration_group_names', implode(", " , $registrationGroupNames));
-
-    return;// $this->group_contact_get($contact_id);
-  }
-
-  function group_contact_get($contact_id){ 
-    
-    $params = array(
-      'contact_id' => $contact_id,
-    );
-
-    try{
-      $result = civicrm_api3('group_contact', 'get', $params);
-    }
-    catch (CiviCRM_API3_Exception $e) {
-      // handle error here
-      $errorMessage = $e->getMessage();
-      $errorCode = $e->getErrorCode();
-      $errorData = $e->getExtraParams();
-      return array('error' => $errorMessage, 'error_code' => $errorCode, 'error_data' => $errorData);
-    }
-
-    return $result;
   }
 }
--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/templates/CRM/Tournament/Page/Registration.tpl
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/templates/CRM/Tournament/Page/Registration.tpl
@@ -1,18 +1,30 @@
 {* Displays Registration Control Panel *}
 <div id="help" class="description section-hidden-border">
-{capture assign=plusImg}<img src="{$config->resourceBase}i/TreePlus.gif" alt="{ts}plus sign{/ts}" style="vertical-align: bottom; height: 20px; width: 20px;" />{/capture}
+{capture assign=plusImg}<img src="{$config->resourceBase}i/TreePlus.gif" alt="{ts}plus sign{/ts}" />{/capture}
 {ts 1=$plusImg}Administer your organization's tournament registration using the links on this page. {* Click %1 for descriptions of the options in each section.*}{/ts}
 </div>
 {strip}
 <div class="crm-content-block">
-<h3>Upcoming Tournament: {$billing_contact->tournament->event->title}</h3>
+<h3>Upcoming Tournament: {$billing_contact->tournament->event.title}</h3>
 
-<h4>Welcome <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->contact->id}" target="_blank">{$billing_contact->contact->display_name}</a>.</h4>
+<h4>Welcome <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->contact.id}" target="_blank">{$billing_contact->contact.display_name}</a>.</h4>
 <ul>
-<li>Update <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->contact->id}" target="_blank">your contact information</a>.</li>
-<li>Update <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->organization->id}" target="_blank">your organization's contact information</a> ({$billing_contact->organization->display_name}).</li>
-<!--li>Update <a href="admin.php?page=CiviCRM&q=civicrm/contact/view/rel&reset=1&cid=1004" target="_blank">Ann Arbor participants</a>.</li-->
-<li>Register participants for <a href="admin.php?page=CiviCRM&q=civicrm/event/register&reset=1&id={$billing_contact->tournament->event->id}" target="_blank">{$billing_contact->tournament->event->title}</a>.</li>
+<li>Update <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->contact.id}" target="_blank">your individual contact information</a>.</li>
+<li>Update <a href="admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$billing_contact->organization.id}" target="_blank">your organization's contact information</a> ({$billing_contact->organization.display_name}).</li>
+<li>Register participants for <a href="admin.php?page=CiviCRM&q=civicrm/event/register&reset=1&id={$billing_contact->tournament->event.id}" target="_blank">{$billing_contact->tournament->event.title}</a>.</li>
 </ul>
+
+{assign var="rows" value=$billing_contact->members}
+{if $rows}
+	 {* This section displays the rows along and includes the paging controls *}
+	 <div class="crm-search-results">
+    {include file='CRM/Contact/Form/Selector.tpl' context='Search'}
+	 </div> 
+{else}
+  <div class="crm-results-block crm-results-block-empty">
+      {include file="CRM/Tournament/Form/Search/EmptyMemberResults.tpl"}
+  </div>
+{/if}
+
 </div>
 {/strip}
