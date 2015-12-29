14d4361d4b5089973503977ae69f332775cec26b
agloa <cio@agloa.org>
2015-12-29 14:15:04 -0600

    simple table of members linked to profile

--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/CRM/Tournament/Page/Registration.php
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/CRM/Tournament/Page/Registration.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once 'CRM/Core/Page.php';
-require_once 'api/class.api.php';
-
+require_once 'CRM/Tournament/TournamentObject.php';
 
 class Tournament extends TournamentObject {
 	/**
@@ -82,121 +80,12 @@ class BillingContact extends TournamentObject{
 		foreach ($relations as $relation) return $this->get_contact($relation['contact_id_b']);
 	}
 }
-class TournamentObject{
-	/**
-	 *
-	 *
-	 * @var bool
-	 */
-	public $is_error;
-	/**
-	 *
-	 *
-	 * @var string
-	 */
-	public $error_message;
-
-	function __construct(){
-		$this->is_error = 0;
-	}
-
-	function tournament_event_type_id(){
-		return $this->option_value('event_type', 'Tournament');
-	}
-
-	function get_active_relationships($cid = null,  $apiParams) {
-		return $this->get_relationships($cid, 1,  $apiParams);
-	}
-
-	function get_active_b_relationships($cid = null, $apiParams) {
-		return $this->get_relationships($cid, 1, $apiParams, 'contact_id_b');
-	}
-
-	function get_members($organization){
-		$apiParams = array('relationship_type_id' => $this->member_relationship_type_id());
-
-		$relations = $this->get_active_b_relationships($organization['id'], $apiParams);
-		$members = array();
-		foreach ($relations as $relation){
-			$member = $this->get_contact($relation['contact_id_a']);
-			$members[$member['id']] = $member;
-		}
-		return $members;
-	}
-
-	function member_relationship_type_id() {
-		return $this->relationship_type_id('Member of');
-	}
-
-	function get_relationships($cid = null, $is_active = 0, $apiParams, $field = 'contact_id_a') {
-		if (isset($cid)){
-			$apiParams = $this->params_merge($apiParams, array($field => $cid));
-		}
-
-		$apiParams = $this->params_merge($apiParams, array('is_active' => $is_active));
-		return $this->civicrm_api3_get('Relationship', $apiParams);
-	}
-
-	function option_value($groupName, $name){
-		$groupID = $this->option_group_id($groupName);
-		$apiParams = array('option_group_id' => $groupID, 'name' => $name);
-		$table = 'Option Value';
-		$record = $this->civicrm_api3_get($table, $apiParams, 'getsingle');
-		return $record['value'];
-	}
-
-	function option_group_id($name){
-		$apiParams = array('name' => $name);
-		return $this->table_name_to_id('Option Group', $name);
-	}
-
-	function relationship_type_id($name){
-		$record = $this->civicrm_api3_get('Relationship Type', array('name_a_b' => $name), 'getsingle');
-		return $record['id'];
-	}
-
-	function table_name_to_id($table, $name, $field = 'name', $params = null){
-		$apiParams = $this->params_merge($params, array($field => $name));
-		return $this->table_record_id($table, $apiParams);
-	}
-
-	function table_record_id($table, $apiParams){
-		$record = $this->civicrm_api3_get($table, $apiParams, 'getsingle');
-		return $record['id'];
-	}
-
-	function get_contact($contact_id){
-		return $this->civicrm_api3_get('Contact', array('id' => $contact_id), 'getsingle');
-	}
-
-	function civicrm_api3_get($tableName, $apiParams, $action = 'get') {
-		try{
-			if ($action == 'getsingle')
-				$apiParams['options'] = $this->params_merge($apiParams['options'], array('limit' => 1));
-				
-			$result = civicrm_api3($tableName, $action, $apiParams);
-			if ($action == 'getsingle') return $result;
-			return $result[values];
-		}
-		catch (CiviCRM_API3_Exception $e) {
-			// handle error here
-			$errorMessage = $e->getMessage();
-			$errorCode = $e->getErrorCode();
-			$errorData = $e->getExtraParams();
-			return array('error' => $errorMessage, 'error_code' => $errorCode, 'error_data' => $errorData);
-		}
-	}
-
-	function params_merge($oldParams, $newParams){
-		if (isset($oldParams)) return array_merge($oldParams, $newParams);
-		else return $newParams;
-	}
-}
 
 class CRM_Tournament_Page_Registration extends CRM_Core_Page {
 	public $billing_contact;
 	
   public function run() {
+//  	$smarty->debugging_ctrl = ($_SERVER['SERVER_NAME'] == 'localhost') ? 'URL' : 'NONE';
     // billing contact is current user, unless admin is using cid argument
     if (isset($_REQUEST["cid"])) {
     	$billing_contact_id = $_REQUEST["cid"];
@@ -205,40 +94,53 @@ class CRM_Tournament_Page_Registration extends CRM_Core_Page {
     	$session = CRM_Core_Session::singleton();
     	$billing_contact_id = $session->get('userID');
     }
-    
+
     $this->billing_contact = new BillingContact($billing_contact_id); //var_dump($billing_contact); //die('128');
     if ($this->billing_contact->is_error) var_dump($this->billing_contact);
      
     $this->assign('billing_contact', $this->billing_contact);
     
-    // uf_group is_active=1 name = Billing Organization Profile
+    $baseURL = "admin.php?page=CiviCRM&q=";
     $indProfile = $this->getProfileID('Billing Individual Profile');
     if (isset($indProfile))
-    	$this->assign('indEditLink', 
-    		"admin.php?page=CiviCRM&q=civicrm/profile/edit&reset=1&id={$this->billing_contact->contact['id']}&gid={$indProfile}");
+    	$this->assign('indEditLink', $baseURL.
+    		"civicrm/profile/edit&reset=1&id={$this->billing_contact->contact['id']}&gid={$indProfile}");
     else
-    	$this->assign('indEditLink', 
-    		"admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$this->billing_contact->contact['id']}");
+    	$this->assign('indEditLink', $baseURL.
+    		"civicrm/contact/view&reset=1&cid={$this->billing_contact->contact['id']}");
     
     $orgProfile = $this->getProfileID('Billing Organization Profile');
     if (isset($orgProfile))
-    	$this->assign('orgEditLink', 
-    		"admin.php?page=CiviCRM&q=civicrm/profile/edit&reset=1&id={$this->billing_contact->organization['id']}&gid={$orgProfile}");
+    	$this->assign('orgEditLink', $baseURL.
+    		"civicrm/profile/edit&reset=1&id={$this->billing_contact->organization['id']}&gid={$orgProfile}");
     else
-    	$this->assign('orgEditLink',
-    		"admin.php?page=CiviCRM&q=civicrm/contact/view&reset=1&cid={$this->billing_contact->organization['id']}");
+    	$this->assign('orgEditLink', $baseURL.
+    		"civicrm/contact/view&reset=1&cid={$this->billing_contact->organization['id']}");
     
-    $memberProfile = $this->getProfileID('MemberProfile');
+    // TODO: how to get search results (members) to use this profile?
+    $memberProfile = $this->getProfileID('Member Profile');
     if (isset($memberProfile)){
     	$this->assign('pMemberAdd',"civicrm/profile/create");
     	$this->assign('qMemberAdd', "gid={$memberProfile}&reset=1");
+    	$this->assign('memberEditLink', $baseURL.
+    		"civicrm/profile/edit&gid={$memberProfile}&reset=1");
     }
     else{
     	$this->assign('pMemberAdd',"civicrm/contact/add");
     	$this->assign('qMemberAdd', "ct=Individual&reset=1");
+    	$this->assign('memberEditLink', $baseURL.
+    		"civicrm/contact/edit");
     }
     
-
+		$this->columnHeaders = array();
+		$this->columnHeaders['sort_name'] = array(
+					'name' =>	'Member'
+				, 'field_name' => 'sort_name'
+				, 'sort' =>	'sort_name'
+				, 'direction' => 1
+				);
+		
+		$this->assign('columnHeaders', $this->columnHeaders);
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     //CRM_Utils_System::setTitle(ts('Registration'));
     parent::run();
--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/templates/CRM/Tournament/Page/Registration.tpl
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/templates/CRM/Tournament/Page/Registration.tpl
@@ -12,14 +12,47 @@
 <ul>
 <li>Update <a href={$indEditLink} target="_blank">your individual contact information</a>.</li>
 <li>Update <a href={$orgEditLink} target="_blank">your organization's contact information</a> ({$billing_contact->organization.display_name}).</li>
-<li>Register participants for <a href="civicrm/event/register&reset=1&id={$billing_contact->tournament->event.id}" target="_blank">{$billing_contact->tournament->event.title}</a>.</li>
+{* <li>Register participants for <a href="civicrm/event/register&reset=1&id={$billing_contact->tournament->event.id}" target="_blank">{$billing_contact->tournament->event.title}</a>.</li>*}
 </ul>
-These members have participated with {$billing_contact->organization.display_name} in the past.
+<p>These members (below) are currently 'related' to {$billing_contact->organization.display_name}.</P>
+
+  {capture assign=crmURLI}{crmURL p=$pMemberAdd q=$qMemberAdd}{/capture}
+  {ts 1=$crmURLI}Add a <a href='%1' target='_blank'>New Member</a>{/ts} for {$billing_contact->organization.display_name}.
+
 {assign var="rows" value=$billing_contact->members}
 {if $rows}
 	 {* This section displays the rows along and includes the paging controls *}
 	 <div class="crm-search-results">
-    {include file='CRM/Contact/Form/Selector.tpl'}
+	 
+	     <table>
+      <tr class="columnheader">
+      {foreach from=$columnHeaders item=header}
+        <th scope="col">
+        {*if $header.sort}
+          {assign var='key' value=$header.sort}
+          {$sort->_response.$key.link}
+        {else*}
+          {$header.name}
+        {*/if*}
+         </th>
+      {/foreach}
+      </tr>
+
+      {counter start=0 skip=1 print=false}
+      {foreach from=$rows item=row name=listings}
+      	<tr id="row-{$smarty.foreach.listings.iteration}" class="{cycle values="odd-row,even-row"}">
+      		{foreach from=$row key=index item=value}
+        		{if $columnHeaders.$index.field_name}
+          		<td class="crm-{$columnHeaders.$index.field_name}"><a href="{$memberEditLink}&id={$row.id}">{$value}</a></td>
+        		{*else}
+          		<td>{$value}</td>*}
+        		{/if}
+      	{/foreach}
+      	</tr>
+      {/foreach}
+    </table>
+    
+    {*include file='CRM/Contact/Form/Selector.tpl'*}
 	 </div> 
 {else}
   <div class="crm-results-block crm-results-block-empty">
