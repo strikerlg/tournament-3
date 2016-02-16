9047a84fa3c475c5c0a082e8f6b07d49bd9f0beb
agloa <cio@agloa.org>
2016-02-16 06:35:20 -0600

    clone Event/Form/Participant.php to Tournament/Form/Participant.php as a starting point for customization

--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/tournament.php
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/tournament.php
@@ -8,7 +8,310 @@
  */
 require_once 'tournament.civix.php';
 require_once 'util.php';
-//require_once 'CRM/Tournament/TournamentObject.php';
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ */
+function tournament_civicrm_navigationMenu(&$menu) {
+	$domain = array('domain' => domain());
+	$path = path();
+	$name = $path;
+	// This inserts a menu at the end the bar. TODO: How to insert at beginning?
+	_tournament_civix_insert_navigation_menu($menu, NULL, array(
+			'label' => ts('Tournament', $domain), 'name' => $name, 'url' => ''));
+
+	$path = $name;
+	$name = 'tournament_dashboard';
+	_tournament_civix_insert_navigation_menu($menu, $path, array(
+			'label' => ts('Dashboard', $domain),
+			'name' => $name,
+			'url' => 'civicrm',
+			'permission' => 'access Contact Dashboard',
+	));
+
+	$name = 'individual_profile';
+
+	$billing_contact = billing_contact_get();
+	$billing_contact_id = $billing_contact["id"];
+
+	$record = named_profile_get("Billing Individual Profile");
+	$gid = $record["id"];
+	$delim = '?';
+	
+	$HREF = contact_profile_HREF_data($billing_contact, $record, $delim);
+	$label = $HREF['title'];
+	$url = $HREF['relativeURL'];
+
+	_tournament_civix_insert_navigation_menu($menu, $path, array(
+			'label' => ts($label, $domain),
+			'name' => $name,
+			'url' => $url,
+			'permission' => 'edit my contact',
+	));
+
+ 	$name = 'organization_profile';
+	_tournament_civix_insert_navigation_menu($menu, $path, array(
+			'label' => ts("Organizations", $domain),
+			'name' => $name,
+			'url' => '',
+	));
+	
+	$path .= "/{$name}";
+	
+	$profile = named_profile_get("Billing Organization Profile");
+	$billing_organizations = billing_organizations_get($billing_contact_id);
+	foreach ($billing_organizations as $org){		
+		$HREF = contact_profile_HREF_data($org, $profile, $delim);
+		$label = $HREF['title'];
+		$url = $HREF['relativeURL'];
+
+		_tournament_civix_insert_navigation_menu($menu, $path, array(
+				'label' => ts($label, $domain),
+				'name' => "{$name}_{$organization_name}",
+				'url' => $url,
+				'permission' => 'access contact reference fields',
+				));
+	}
+	
+	$path = path();
+	$name = 'Profiles';
+	_tournament_civix_insert_navigation_menu($menu, $path, array(
+			'label' => ts('Your players, coaches, etc.', $domain),
+			'name' => $name,
+			'permission' => 'profile edit',
+	));
+
+	//$path .= "/{$name}";
+
+	// add a menu item for each of the sesstion billing contact's profiles
+//	$registrationProfiles = $session->get('registrationProfiles');
+// 	foreach ($registrationProfiles as $profile) {
+// 		$id = $profile["id"];
+// 		$title = $profile["title"];
+// 		_tournament_civix_insert_navigation_menu($menu, $path, array(
+// 				'label' => ts($title, array('domain' => domain())),
+// 				'name' => "{$name}_{$id}",
+// 				'permission' => 'profile edit',
+// 				));
+// 		_tournament_civix_insert_navigation_menu($menu, "{$path}/Profiles/Profiles_{$id}", array(
+// 				'label' => ts("List", array('domain' => domain())),
+// 				'name' => "{$name}_{$id}_list",
+// 				'url' => "civicrm/profile?gid={$id}&reset=1&force=1",
+// 				'permission' => 'profile edit',
+// 				));
+// 		_tournament_civix_insert_navigation_menu($menu, "{$path}/Profiles/Profiles_{$id}", array(
+// 				'label' => ts("Add new", array('domain' => domain())),
+// 				'name' => "{$name}_{$id}_add",
+// 				'url' => "civicrm/profile/create?gid={$id}&reset=1",
+// 				'permission' => 'profile create',
+// 				));
+// 	}
+
+	_tournament_civix_insert_navigation_menu($menu, $path, array(
+			'label' => ts('Register Participants', $domain),
+			'name' => 'registration',
+			'url' => "civicrm/tournament/participant/add?reset=1&action=add&context=standalone&eid=1",
+			'permission' => 'administer civicrm',
+			// 			'permission' => 'edit event participants',
+			'separator' => 1,
+	));
+
+// 	$record = named_profile_get("Billing Organization Profile");
+// 	$id = $record["id"];
+// 	_tournament_civix_insert_navigation_menu($menu, path(), array(
+// 			'label' => ts('Billing Organizations', array('domain' => $domain)),
+// 			'name' => 'BillingOrganizations',
+// 			'url' => "civicrm/profile?gid={$id}",
+// 			'permission' => 'view all contacts',
+// 			));
+
+// 	$record = named_report_get("Preliminary Estimates Summary");
+// 	$id = $record["id"];
+// 	_tournament_civix_insert_navigation_menu($menu, path()."/BillingOrganizations", array(
+// 			'label' => ts('Preliminary Estimates', array('domain' => $domain)),
+// 			'name' => 'PreliminaryEstimates',
+// 			'url' => "civicrm/report/instance/{$id}?reset=1",
+// 			'permission' => 'view all contacts',
+// 			));
+
+// 	$record = named_group_get("Billing Contacts");
+// 	$id = $record["id"];
+// 	_tournament_civix_insert_navigation_menu($menu, path(), array(
+// 			'label' => ts('Billing Individuals', array('domain' => $domain)),
+// 			'name' => 'BillingContacts',
+// 			'url' => "civicrm/group/search?context=smog&gid={$id}&reset=1&force=1",
+// 			'permission' => 'view all contacts',
+// 			));
+
+	_tournament_civix_navigationMenu($menu);
+}
+
+/**
+ * Implements hook_civicrm_dashboard().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_dashboard
+ */
+function tournament_civicrm_dashboard( $contactID, &$contentPlacement ) {	
+	// Insert custom content above activities
+	$contentPlacement = CRM_Utils_Hook::DASHBOARD_ABOVE;
+	
+	$cid = CRM_Utils_Request::retrieve('cid', 'Positive', $this);
+	if (!isset($cid)) $cid = $contactID;
+	$contact = contact_get($cid);
+	$profile = named_profile_get("Billing Individual Profile");
+	$currentUserHREF = profileEditHREF($profile, $contact);
+	$currentUserHTML = "Welcome, {$currentUserHREF}.";
+	
+	$billingOrgsHTML = "You are a contact for these organizations:<ol>";
+	$billingOrgs = billing_organizations_get($cid);	
+	$profile = named_profile_get("Billing Organization Profile");
+	foreach($billingOrgs as $contact) {		
+		$billingOrgsHTML .= "<li>" . profileEditHREF($profile, $contact) . "</li>";
+	}
+	$billingOrgsHTML .= "</ol>";
+	
+	$registrationProfilesHTML = "You have access to contacts in these groups:<ol>";
+	$registrationProfiles = get_registrationProfiles($cid);	
+	foreach($registrationProfiles as $profile) {
+		$registrationProfilesHTML .= "<li>" . $profile['title'] . "<ul>";
+		$registrationProfilesHTML .= "<li>" . profileListHREF($profile) . "</li>";
+		$registrationProfilesHTML .= "<li>" . profileCreateHREF($profile) . "</li>";
+		$registrationProfilesHTML .= "</li></ul>";
+	}
+	$registrationProfilesHTML .= "</ol>";
+	
+	return array( 'Current User' => $currentUserHTML,
+			'Billing Organizations' => $billingOrgsHTML,
+			'Contacts (Players, coaches, etc.)' => $registrationProfilesHTML,
+	);
+}
+
+/**
+ * billing contact is current user, unless admin is using cid argument
+ *
+ *
+ * @return long
+ */
+function billing_contact_get(){
+	$session = CRM_Core_Session::singleton();
+	if (null == $session->get('billing_contact_id')) {
+		if (isset($_REQUEST["cid"])) {
+			$billing_contact_id = $_REQUEST["cid"];
+		} else {
+			// Get logged in user
+			$billing_contact_id = $session->get('userID');
+		}
+		$session->set('billing_contact_id', $billing_contact_id);
+	}
+	$billing_contact_id = $session->get('billing_contact_id');
+	return contact_get($billing_contact_id);
+}
+
+/**
+ * Get orgs for which $contactID is billing contact
+ *
+ *
+ * @param $name string
+ * @return array
+ */
+function billing_organizations_get($contactID){
+	$apiParams = array('relationship_type_id' => relationship_type_id('Billing Contact for'));
+	$relations = get_active_relationships($contactID, $apiParams);
+	//$orgs = array();
+	foreach ($relations as $relation) {
+		$orgID = $relation['contact_id_b'];
+		$orgs[$orgID] = contact_get($orgID);
+	}
+	return $orgs;
+}
+
+/**
+ * Get registration profiles for $contactID registration groups
+ *
+ * @param $contact_id long
+ * @return array
+ */
+function get_registrationProfiles($contact_id){
+	$aclGroups = get_aclGroups($contact_id);
+	$registrationGroups = get_registrationGroups($aclGroups);
+
+	foreach ($registrationGroups as $group){
+		$group_id = $group["id"];
+		$apiParams = array("add_to_group_id" => $group_id
+				, "limit_listings_group_id" => $group_id
+				, "group_type" => "Individual"
+				, "is_active" => 1
+				, 'options' => array( 'sort' => 'created_date DESC', 'limit' => 1)
+		);
+
+		$entity_name = "uf_group";
+		$result = civicrm_api3_get($entity_name, $apiParams, 'getsingle');
+
+		$profiles[$result["id"]] = $result;
+	}
+
+	return $profiles;
+}
+
+function get_registrationGroups($aclGroups){ // test 93
+	$roles = get_aclRoles($aclGroups);
+	foreach ($roles as $role){
+		$entity_id = $role["acl_role_id"]; // should be 39
+		$apiParams = array("entity_id" => $entity_id
+				, "deny" => 0, "object_table" => "civicrm_saved_search"
+				, "entity_table" => "civicrm_acl_role", "is_active" => 1);
+
+		$entity_name = "Acl";
+		$result = civicrm_api3_get($entity_name, $apiParams); // object_id should be 56
+
+		$key = "id";
+		if (is_array($result)) foreach ($result as $record) $records[$record[$key]] = $record;
+		else $records[$result[$key]] = $result;
+	}
+
+	$acls = $records;
+	unset($records);
+	unset($result);
+
+	$key = "id";
+	foreach($acls as $acl) {
+		$result = civicrm_api3_get('Group', array($key => $acl["object_id"]));
+
+		if (is_array($result)) foreach ($result as $record) $records[$record[$key]] = $record;
+		else $records[$result[$key]] = $result;
+	}
+
+	return $records;
+}
+
+function domain(){
+	return 'org.agloa.tournament';
+}
+function path(){
+	return 'tournament';
+}
+
+/**
+ * Implementation of hook_civicrm_preProcess
+ */
+function tournament_civicrm_preProcess($formName, &$form){
+	switch ($formName) {
+		case 'Contribution':
+			$file = '/tmp/contributions.log';
+			$message = strtr("Performed \"@op\" at @time on contribution #@id\n", array(
+					'@op' => $op,
+					'@time' => date('Y-m-d H:i:s'),
+					'@id' => $objectId,
+			));
+			file_put_contents($file, $message, FILE_APPEND);
+			break;
+		default:
+			// nothing to do
+	}
+}
+
 /**
  * Implementation of hook_civicrm_post
  */
@@ -28,11 +331,11 @@ function tournament_post($op, $objectName, $objectId, &$objectRef) {
 	}
 }
 
-/**
- * Implements hook_civicrm_config().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
- */
+	/**
+	 * Implements hook_civicrm_config().
+	 *
+	 * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+	 */
 function tournament_civicrm_config(&$config) {
 	_tournament_civix_civicrm_config($config);
 }
@@ -146,152 +449,4 @@ function tournament_civicrm_angularModules(&$angularModules) {
  */
 function tournament_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
 	_tournament_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-function tournament_civicrm_navigationMenu(&$menu) {
-  $session = CRM_Core_Session::singleton();
-	$path = NULL;
-	// 'civicrm'; //NULL
-
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Tournament', array('domain' => 'org.agloa.tournament')),
-			'name' => 'tournament',
-			'url' => '',
-	));
-
-	$path = 'tournament';
-	$url = 'civicrm/' . $path;
-
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Dashboard', array('domain' => 'org.agloa.tournament')),
-			'name' => 'tournament_dashboard',
-			'url' => $url,
-			'permission' => 'access Contact Dashboard',
-	));
-	
-	$billing_contact_id = $session->get('billing_contact_id');
-	$record = named_profile_get("Billing Individual Profile");
-	$id = $record["id"];
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Your Individual Contact Information', array('domain' => 'org.agloa.tournament')),
-			'name' => 'individual_profile',
-			'url' => "civicrm/profile/edit?reset=1&id={$billing_contact_id}&gid={$id}",
-			'permission' => 'edit my contact',
-	));
-	
-	$organization_id = $session->get('billing_org_id');
-	$organization_name = $session->get('billing_org_name');
-	$record = named_profile_get("Billing Organization Profile");
-	$id = $record["id"];
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts("{$organization_name} Contact Information", array('domain' => 'org.agloa.tournament')),
-			'name' => 'organization_profile',
-			'url' => "civicrm/profile/edit?reset=1&id={$organization_id}&gid={$id}",
-			'permission' => 'access contact reference fields',
-	));
-
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Your players, coaches, etc.', array('domain' => 'org.agloa.tournament')),
-			'name' => "Profiles",
-			'permission' => 'profile edit',
-	));
-
-	// add a menu item for each of the sesstion billing contact's profiles
-	$registrationProfiles = $session->get('registrationProfiles');
-	foreach ($registrationProfiles as $profile) {
-		$id = $profile["id"];
-		$title = $profile["title"];
-		_tournament_civix_insert_navigation_menu($menu, "{$path}/Profiles", array(
-				'label' => ts($title, array('domain' => 'org.agloa.tournament')),
-				'name' => "Profiles_{$id}",
-			'permission' => 'profile edit',
-				));
-		_tournament_civix_insert_navigation_menu($menu, "{$path}/Profiles/Profiles_{$id}", array(
-				'label' => ts("List", array('domain' => 'org.agloa.tournament')),
-				'name' => "Profiles_{$id}_list",
-				'url' => "civicrm/profile?gid={$id}&reset=1&force=1",
-			'permission' => 'profile edit',
-				));
-		_tournament_civix_insert_navigation_menu($menu, "{$path}/Profiles/Profiles_{$id}", array(
-				'label' => ts("Add new", array('domain' => 'org.agloa.tournament')),
-				'name' => "Profiles_{$id}_add",
-				'url' => "civicrm/profile/create?gid={$id}&reset=1",
-			'permission' => 'profile create',
-				));
-	}
-	
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Register Participants', array('domain' => 'org.agloa.tournament')),
-			'name' => 'registration',
-			'url' => "civicrm/tournament/participant/add?reset=1&action=add&context=standalone",			
-			'permission' => 'edit event participants',
-			'separator' => 1,
-			));
-	
-	$record = named_profile_get("Billing Organization Profile");
-	$id = $record["id"];
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Billing Organizations', array('domain' => 'org.agloa.tournament')),
-			'name' => 'BillingOrganizations',
-			'url' => "civicrm/profile?gid={$id}",
-			'permission' => 'view all contacts',
-			));
-	
-	$record = named_report_get("Preliminary Estimates Summary");
-	$id = $record["id"];
-	_tournament_civix_insert_navigation_menu($menu, "{$path}/BillingOrganizations", array(
-			'label' => ts('Preliminary Estimates', array('domain' => 'org.agloa.tournament')),
-			'name' => 'PreliminaryEstimates',
-			'url' => "civicrm/report/instance/{$id}?reset=1",
-			'permission' => 'view all contacts',
-			));
-	
-	$record = named_group_get("Billing Contacts");
-	$id = $record["id"];
-	_tournament_civix_insert_navigation_menu($menu, $path, array(
-			'label' => ts('Billing Individuals', array('domain' => 'org.agloa.tournament')),
-			'name' => 'BillingContacts',
-			'url' => "civicrm/group/search?context=smog&gid={$id}&reset=1&force=1",
-			'permission' => 'view all contacts',
-			));
-
-	_tournament_civix_navigationMenu($menu);
-}
-
-function tournament_civicrm_dashboard( $contactID, &$contentPlacement ) {
-	// $uploadDir = $config->uploadDir;
-	// Insert custom content above activities
-	$contentPlacement = CRM_Utils_Hook::DASHBOARD_ABOVE;
-	$cid = CRM_Utils_Request::retrieve('cid', 'Positive', $this);
-	if (!isset($cid)) $cid = $contactID;
-	$contact = contact_get($contactID);
-	$displayName = $contact['display_name'];
-	$title = "Use this link to edit your contact record.";
-	return array( 'Current User' => "<p>cid: $cid</p><p>Welcome, <a title=\"$title\" href=\"/civicrm/contact/add&reset=1&action=update&cid={$contactID}\">$displayName</a>.</p>",
-			'Custom Table' => "
-			<table>
-			<tr><th>Contact Name</th><th>Date</th></tr>
-			<tr><td>Foo</td><td>Bar</td></tr>
-			<tr><td>Goo</td><td>Tar</td></tr>
-			</table>
-			",
-	);
-}
-/**
- * Implementation of hook_civicrm_preProcess
- */
-function tournament_civicrm_preProcess($formName, &$form){
-	switch ($formName) {
-		case 'Contribution':
-			$file = '/tmp/contributions.log';
-			$message = strtr("Performed \"@op\" at @time on contribution #@id\n", array(
-					'@op' => $op,
-					'@time' => date('Y-m-d H:i:s'),
-					'@id' => $objectId,
-			));
-			file_put_contents($file, $message, FILE_APPEND);
-			break;
-		default:
-			// nothing to do
-	}
 }
--- wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/util.php
+++ wordpress/wp-content/plugins/files/civicrm/custom_ext/org.agloa.tournament/util.php
@@ -1,7 +1,78 @@
 <?php
 /**
- * get contact matching id
+ * get acl groups a contact is member of
  *
+ * @param $contact_id long
+ * @param $status string
+ * @return array
+ */
+function get_aclGroups($contact_id, $is_active = 1, $params){
+	$contact_groups = get_contact_groups($contact_id);
+
+	foreach ($contact_groups as $contact_group) {
+		$groupID = $contact_group["group_id"];
+		$apiParams = params_merge($params,
+				array("id" => $groupID, "group_type" => access_control_group_type_id()
+						, "is_active" => $is_active, "is_hidden" => "0"));
+			
+		$entity_name = "Group";
+		$result = civicrm_api3_get($entity_name, $apiParams);
+			
+		if (is_array($result)) foreach ($result as $group) $groups[$group["id"]] = $group;
+		else $groups[$result["id"]] = $result;
+	}
+
+	return $groups;
+}
+
+/**
+ * get groups a contact is member of
+ * 
+ * @param $contact_id long
+ * @param $status string
+ * @return array
+ */
+function get_contact_groups($contact_id, $status = "Added"){
+	$apiParams = array("contact_id" => $contact_id, "status" => $status);
+	return civicrm_api3_get('Group_Contact', $apiParams);
+}
+
+/**
+ * get acl roles groups
+ *
+ * @param $aclGroups array
+ * @return array
+ */
+function get_aclRoles($aclGroups){
+	foreach ($aclGroups as $group){
+		$entity_id = $group["id"];
+		$apiParams = array("entity_id" => $entity_id
+				, "entity_table" => "civicrm_group", "is_active" => 1);
+			
+		$entity_name = "Acl_Role";
+		$result = civicrm_api3_get($entity_name, $apiParams);
+			
+		if (is_array($result)) foreach ($result as $record) $records[$record["id"]] = $record;
+		else $records[$result["id"]] = $result;
+	}
+
+	return $records;
+}
+
+/**
+ * get civicrm base URL
+ *
+ * @return string
+ */
+function baseURL(){
+	$config = CRM_Core_Config::singleton();
+	$userFrameworkBaseURL = rtrim($config->userFrameworkBaseURL, "/");
+	$civiURL =  $config->civiRelativeURL;
+	return "{$userFrameworkBaseURL}{$civiURL}";
+}
+
+/**
+ * get contact matching id
  *
  * @param $id long
  * @return array
@@ -10,6 +81,139 @@ function contact_get($id){
 	$result = civicrm_api3('contact', 'get', array("id" => $id));
 	return $result['values'][$id];
 }
+
+/**
+ * get HREF to profile listing
+ *
+ * @param $profile array
+ * @param $contact array
+ * @return string
+ */
+function profileEditHREF($profile, $contact){
+	$HREF = contact_profile_HREF_data($contact, $profile);
+	//$cid = $contact['id'];
+	$text = $HREF['text'];//$contact['display_name'];
+	$title = $HREF['title'];//"Use this link to edit $text";
+	$token = $HREF['relativeURL'];//"civicrm/profile/edit&reset=1&=1&id={$cid}&gid=";
+	return tokenIDHREF($token, $title, $text);
+}
+
+/**
+ * get HREF to profile listing
+ *
+ * @param $profile array
+ * @return string
+ */
+function profileListHREF($profile){
+	$text = 'List';
+	$title = "Use this link to edit contacts in {$profile['title']}";
+	$token = "civicrm/profile&reset=1&force=1&gid=";
+	return tokenIDHREF($token, $profile["id"], $title, $text);
+}
+
+/**
+ * get HREF to create with profile
+ *
+ * @param $profile array
+ * @return string
+ */
+function profileCreateHREF($profile){
+	$title = "Use this link to add contacts to {$profile['title']}";
+	return tokenIDHREF("civicrm/profile/create&reset=1&gid=", $profile["id"], $title, 'Add New');
+}
+
+/**
+ * build HREF from base URL, token, record id, link title, and record name
+ *
+ * @param $token string
+ * @param $title string
+ * @param $id long
+ * @param $name string
+ * @return string
+ */
+function tokenIDHREF($token, $title, $name){
+	$baseURL = baseURL();
+	return "<a title=\"$title\" href=\"{$baseURL}{$token}\">$name</a>";
+}
+
+/**
+ * build HREF from contact & profile
+ *
+ * @param $contact array
+ * @param $profile array
+ * @return array
+ */
+function contact_profile_HREF_data($contact, $profile, $delim='&'){
+	$cid = $contact['id'];
+	$gid = $profile['id'];
+	$text = $contact['display_name'];
+	$HREF['text'] = $text;
+	$HREF['title'] = "Use this link to edit {$text}";
+	$HREF['relativeURL'] = "civicrm/profile/edit{$delim}reset=1&id={$cid}&gid={$gid}";
+	return $HREF;
+}
+
+/**
+ * get id for Access Control group type
+ *
+ * @return long
+ */
+function access_control_group_type_id() {
+	return option_value("group_type", "Access Control");
+}
+
+/**
+ * get id for for a named option value
+ *
+ * @param $groupName string
+ * @param $name string
+ * @return long
+ */
+function option_value($groupName, $name){
+	$groupID = option_group_id($groupName);
+	$apiParams = array('option_group_id' => $groupID, 'name' => $name);
+	$table = 'Option Value';
+	$record = civicrm_api3_get($table, $apiParams, 'getsingle');
+	return $record['value'];
+}
+
+/**
+ * get id for for a named option group
+ *
+ * @param $name string
+ * @return long
+ */
+function option_group_id($name){
+	$apiParams = array('name' => $name);
+	return table_name_to_id('Option Group', $name);
+}
+
+/**
+ * get id for for a named table field
+ *
+ * @param $table string
+ * @param $name string
+ * @param $field string
+ * @param $params array
+ * @return long
+ */
+ function table_name_to_id($table, $name, $field = 'name', $params = null){
+	$apiParams = params_merge($params, array($field => $name));
+	return table_record_id($table, $apiParams);
+}
+
+/**
+ * get id for a record in a table
+ *
+ * @param $table string
+ * @param $apiParams array
+ * @return long
+ */
+function table_record_id($table, $apiParams){
+	$record = civicrm_api3_get($table, $apiParams, 'getsingle');
+	return $record['id'];
+}
+
 /**
  * Search for group which matches title
  *
@@ -54,4 +258,87 @@ function named_record_get($tableName, $title){
 	$apiParams = array("title" => $title);
 	$result = civicrm_api3($tableName, $action, $apiParams);
 	return $result;
+}
+
+/**
+ * Lookup relationship type id from name
+ *
+ *
+ * @param $name string
+ * @return long
+ */
+function relationship_type_id($name){
+	$record = civicrm_api3_get('Relationship Type', array('name_a_b' => $name), 'getsingle');
+	return $record['id'];
+}
+
+/**
+ * 'Get' wrapper around civicrm_api3
+ *
+ *
+ * @param $tableName string
+ * @param $apiParams array
+ * @return array
+ */
+function civicrm_api3_get($tableName, $apiParams, $action = 'get') {
+	try{
+		if ($action == 'getsingle')
+			$apiParams['options'] = params_merge($apiParams['options'], array('limit' => 1));
+
+		$result = civicrm_api3($tableName, $action, $apiParams);
+		if ($action == 'getsingle') return $result;
+		return $result[values];
+	}
+	catch (CiviCRM_API3_Exception $e) {
+		// handle error here
+		$errorMessage = $e->getMessage();
+		$errorCode = $e->getErrorCode();
+		$errorData = $e->getExtraParams();
+		return array('error' => $errorMessage, 'error_code' => $errorCode, 'error_data' => $errorData);
+	}
+}
+
+/**
+ * Get active relationships for a contact id
+ *
+ *
+ * @param $cid long
+ * @param $apiParams array
+ * @param $field string
+ * @return array
+ */
+function get_active_relationships($cid = null,  $apiParams, $field = 'contact_id_a') {
+	return get_relationships($cid, 1,  $apiParams, $field);
+}
+
+/**
+ * Get relationships for a contact id
+ *
+ *
+ * @param $cid long
+ * @param $is_active short
+ * @param $apiParams array
+ * @param $field string
+ * @return array
+ */
+function get_relationships($cid = null, $is_active = 0, $apiParams, $field = 'contact_id_a') {
+	if (isset($cid)){
+		$apiParams = params_merge($apiParams, array($field => $cid));
+	}
+
+	$apiParams = params_merge($apiParams, array('is_active' => $is_active));
+	return civicrm_api3_get('Relationship', $apiParams);
+}
+
+/**
+ * Merge parameter sets
+ *
+ *
+ * @param $oldParams array
+ * @param $newParams array
+ * @return array
+ */
+ function params_merge($oldParams, $newParams){
+	if (isset($oldParams)) return array_merge($oldParams, $newParams);
+	else return $newParams;
 }
